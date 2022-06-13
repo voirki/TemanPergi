ee35705ba23e2ff8913d66c2e313177d45620ef1
iggafebrianv <iggafebrian4@gmail.com>
2022-06-13 10:36:52 +0700

    .

--- temanpergi/lib/main.dart
+++ temanpergi/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: MyApp(),
-));
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
 
 class MyApp extends StatefulWidget {
   @override
@@ -11,34 +11,46 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("TemanPergi",
-        style: new TextStyle(
-          fontSize: 24.0,
-        ),),
-        backgroundColor: Color.fromARGB(255, 170, 94, 187),
-        actions: [
-          PopupMenuButton(
-            child: const Align (
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text('Menu'),
-              ),
+        appBar: AppBar(
+          title: Text(
+            "TemanPergi",
+            style: new TextStyle(
+              fontSize: 24.0,
             ),
-            itemBuilder: (context) => [
-              const PopupMenuItem(child: Text('Home'),value: 1,),
-              const PopupMenuItem(child: Text('Maps'),value: 2,),
-              const PopupMenuItem(child: Text('Places'),value: 3,),
-              const PopupMenuItem(child: Text('About'),value: 4,),
-            ]
           ),
-        ],
-      ),
+          backgroundColor: Color.fromARGB(255, 170, 94, 187),
+          actions: [
+            PopupMenuButton(
+                child: const Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text('Menu'),
+                  ),
+                ),
+                itemBuilder: (context) => [
+                      const PopupMenuItem(
+                        child: Text('Home'),
+                        value: 1,
+                      ),
+                      const PopupMenuItem(
+                        child: Text('Maps'),
+                        value: 2,
+                      ),
+                      const PopupMenuItem(
+                        child: Text('Places'),
+                        value: 3,
+                      ),
+                      const PopupMenuItem(
+                        child: Text('About'),
+                        value: 4,
+                      ),
+                    ]),
+          ],
+        ),
         body: Container(
           child: Padding(
             padding: const EdgeInsets.only(top: 20.0),
@@ -49,11 +61,17 @@ class _MyAppState extends State<MyApp> {
                   padding: const EdgeInsets.all(12.0),
                   child: Container(
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0),),
-                      color: Colors.pink
-                    ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.pink),
                     child: Center(
-                      child: Text("Alam", style: TextStyle(fontSize: 24.0,),),
+                      child: Text(
+                        "Alam",
+                        style: TextStyle(
+                          fontSize: 24.0,
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -61,11 +79,17 @@ class _MyAppState extends State<MyApp> {
                   padding: const EdgeInsets.all(12.0),
                   child: Container(
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0),),
-                        color: Colors.green
-                    ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.green),
                     child: Center(
-                      child: Text("Kuliner", style: TextStyle(fontSize: 24.0,),),
+                      child: Text(
+                        "Kuliner",
+                        style: TextStyle(
+                          fontSize: 24.0,
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -73,11 +97,17 @@ class _MyAppState extends State<MyApp> {
                   padding: const EdgeInsets.all(12.0),
                   child: Container(
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0),),
-                        color: Colors.purple
-                    ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.purple),
                     child: Center(
-                      child: Text("Seni", style: TextStyle(fontSize: 24.0,),),
+                      child: Text(
+                        "Seni",
+                        style: TextStyle(
+                          fontSize: 24.0,
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -85,17 +115,23 @@ class _MyAppState extends State<MyApp> {
                   padding: const EdgeInsets.all(12.0),
                   child: Container(
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0),),
-                        color: Colors.yellow
-                    ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.yellow),
                     child: Center(
-                      child: Text("Lainnya", style: TextStyle(fontSize: 24.0,),),
+                      child: Text(
+                        "Lainnya",
+                        style: TextStyle(
+                          fontSize: 24.0,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
           ),
-        )
-        );  }
+        ));
+  }
 }
