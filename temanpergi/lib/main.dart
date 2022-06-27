b59ebf109629d0bb65732f0c0fed97992e5a5b75
iggafebrianv <iggafebrian4@gmail.com>
2022-06-27 13:58:05 +0700

    .

--- temanpergi/lib/main.dart
+++ temanpergi/lib/main.dart
@@ -60,16 +60,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         leading: Image.asset('assets/images/logoTP.png'),
         title: const Text('TemanPergi'),
         backgroundColor: Color.fromARGB(255, 130, 105, 224),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              showSearch(
-                context: context,
-                delegate: CustomSearchDelegate(),
-              );
-            },
-          ),
+        
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -86,72 +77,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         },
       ),
       body: bodies.elementAt(sel),
-    );
-  }
-}
-
-class CustomSearchDelegate extends SearchDelegate {
-  List<String> searchTerms = [];
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-
-    for (var searched in searchTerms) {
-      if (searched.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(searched);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-
-    for (var searched in searchTerms) {
-      if (searched.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(searched);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
     );
   }
 }
