9e5faa17f3a91e01db10c53b9e4e4f0e1dc1cb03
Manuel Benedict <manuelbenedictsitompul@gmail.com>
2022-06-27 13:20:39 +0700

    .

--- temanpergi/lib/main.dart
+++ temanpergi/lib/main.dart
@@ -2,7 +2,7 @@ import 'home.dart';
 import 'places.dart';
 import 'maps.dart';
 import 'about.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
