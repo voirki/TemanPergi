5bbd2103f9f52a238d69c93fc4439831fe95b061
tsaqibsayyidans <tsaqibsayyidans@student.telkomuniversity.ac.id>
2022-07-03 16:05:11 +0700

    .

--- temanpergi/lib/main.dart
+++ temanpergi/lib/main.dart
@@ -2,6 +2,7 @@ import 'home.dart';
 import 'places.dart';
 import 'maps.dart';
 import 'about.dart';
+import 'splashscreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: judul,
-      home: MyStatefulWidget(),
+      home: SplashScreen(),
     );
   }
 }
--- temanpergi/lib/splashscreen.dart
+++ temanpergi/lib/splashscreen.dart
@@ -1,42 +1,43 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'home.dart';
+import 'main.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
-  _SplashscreenState createState() => _SplashScreenState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
   void initState() {
-    @override
     super.initState();
     splashScreenStart();
   }
-}
 
-splashScreenStart() async {
-  var duration = Duration(seconds: 3); 
-  return Timer(duration,(){ 
-    Navigator.of(context).pushReplacement(MaterialPageRoute (builder: (_){
-      return Home();
-  })); 
-});
-}
 
-@override
-Widget build(BuildContext context) { 
-  return Scaffold(
-    backgroundColor: Colors.white, 
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset("assets/images/logoTP.png", height: 250.0,width: 250.0,
-          ),
-        ],
+  splashScreenStart() async {
+    var duration = Duration(seconds: 3); 
+    return Timer(duration,(){ 
+      Navigator.of(context).pushReplacement(MaterialPageRoute (builder: (_){
+        return MyStatefulWidget();
+      })); 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) { 
+    return Scaffold(
+      backgroundColor: Colors.white, 
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/images/logoTP.png", height: 250.0,width: 250.0,
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
