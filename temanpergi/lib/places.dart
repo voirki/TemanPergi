343465b16a6d99c0b7bb931e1f1ef1882d384a2f
Manuel Benedict <manuelbenedictsitompul@gmail.com>
2022-07-03 22:44:12 +0700

    back

--- temanpergi/lib/places.dart
+++ temanpergi/lib/places.dart
@@ -37,78 +37,8 @@ class Places extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: CarouselSlider(
-              options: CarouselOptions(
-                autoPlay: true,
-                aspectRatio: 21 / 9,
-                enlargeCenterPage: true,
-              ),
-              items: imgList
-                  .map((item) => Container(
-                        child: Container(
-                          margin: EdgeInsets.all(5.0),
-                          child: ClipRRect(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5.0)),
-                              child: Stack(
-                                children: <Widget>[
-                                  Image.network(item,
-                                      fit: BoxFit.cover, width: 500.0),
-                                  Positioned(
-                                    bottom: 0.0,
-                                    left: 0.0,
-                                    right: 0.0,
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        gradient: LinearGradient(
-                                          colors: [
-                                            Color.fromARGB(200, 0, 0, 0),
-                                            Color.fromARGB(0, 0, 0, 0)
-                                          ],
-                                          begin: Alignment.bottomCenter,
-                                          end: Alignment.topCenter,
-                                        ),
-                                      ),
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 10.0, horizontal: 20.0),
-                                    ),
-                                  ),
-                                ],
-                              )),
-                        ),
-                      ))
-                  .toList(),
-            ),
-          ),
-          /*FutureBuilder(
-            future: getPlaces(),
-            builder: (context, snapshot){
-              if (snapshot.hasData){
-                return ListView.builder(
-                  itemCount: data[index],
-                  itemBuilder: (context, index){
-                    return Container(
-                      height: 180,
-                      child: Card(
-                        elevation: 5,
-                        child: Row(
-                          children: [
-                            Text(),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                );
-              } else {
-                return Text('Data Error');
-              }
-            },
-          ),*/
-        ],
+      body: Center(
+        child: Text('Places'),
       ),
     );
   }
